dd67db7907b43924e93cc92347d6dae3aa273f3a
Tee Amornkasemwong <tamornkasemwong@netflix.com>
2024-03-02 19:00:12 -0800

    optimized solutions
    - reduced computational time for part 2 from over 2 minutes to half a millisecond
    - flattened the mapping from seed to location, allowing for a single lookup
    - because each window is monotically increasing, only have to get the lowest seed number in the range (if exists)

--- day05/day05.go
+++ day05/day05.go
@@ -1,13 +1,19 @@
 package day05
 
 import (
+	"errors"
 	"math"
+	"slices"
 	"sort"
 	"strconv"
 	"strings"
 
 	"github.com/TeeAmorn/aoc-go-2023/utils"
 )
+
+/*
+ * PART ONE
+ */
 
 func Part1(inputFileNumber int) (string, error) {
 	opts := utils.InputOptions{Day: 5, FileNumber: inputFileNumber}
@@ -16,6 +22,7 @@ func Part1(inputFileNumber int) (string, error) {
 		return "", err
 	}
 
+	// Get seeds
 	seedsString := strings.Split(lines[0], ": ")
 	seedsString = strings.Split(seedsString[1], " ")
 	seeds := make([]int, 0, len(seedsString))
@@ -24,126 +31,22 @@ func Part1(inputFileNumber int) (string, error) {
 		seeds = append(seeds, seed)
 	}
 
-	allMaps := make([][][]int, 0)
-	currentMap := make([][]int, 0)
-	for i := 3; i < len(lines); {
-		line := lines[i]
+	// Build mapping from seed to location
+	mapping := buildMap(lines)
 
-		if len(line) == 0 || line[0] < '0' || line[0] > '9' {
-			allMaps = append(allMaps, currentMap)
-			currentMap = make([][]int, 0)
-			i += 2
-			continue
-		}
-
-		numsString := strings.Split(line, " ")
-		nums := make([]int, 0, 3)
-		for _, numString := range numsString {
-			num, _ := strconv.Atoi(numString)
-			nums = append(nums, num)
-		}
-		tmp := nums[0]
-		nums[0] = nums[1]
-		nums[1] = tmp
-		currentMap = append(currentMap, nums)
-		i++
-	}
-	allMaps = append(allMaps, currentMap)
-
-	for _, currentMap := range allMaps {
-		sort.SliceStable(currentMap, func(i, j int) bool {
-			return currentMap[i][0] < currentMap[j][0]
-		})
-	}
-
-	location := math.MaxInt
-	for _, val := range seeds {
-		for _, currentMap := range allMaps {
-			index := findLastLessThanOrEqualTo(currentMap, 0, len(currentMap)-1, val)
-			if index != -1 {
-				entryMap := currentMap[index]
-				if val < entryMap[0]+entryMap[2] {
-					val = entryMap[1] + (val - entryMap[0])
-				}
-			}
-		}
-		if val < location {
-			location = val
-		}
-	}
-
-	return strconv.Itoa(location), nil
-}
-
-func Part2(inputFileNumber int) (string, error) {
-	opts := utils.InputOptions{Day: 5, FileNumber: inputFileNumber}
-	lines, err := utils.ReadInput(opts)
-	if err != nil {
-		return "", err
-	}
-
-	seedsString := strings.Split(lines[0], ": ")
-	seedsString = strings.Split(seedsString[1], " ")
-	seeds := make([]int, 0, len(seedsString))
-	for _, seedString := range seedsString {
-		seed, _ := strconv.Atoi(seedString)
-		seeds = append(seeds, seed)
-	}
-
-	allMaps := make([][][]int, 0)
-	currentMap := make([][]int, 0)
-	for i := 3; i < len(lines); {
-		line := lines[i]
-
-		if len(line) == 0 || line[0] < '0' || line[0] > '9' {
-			allMaps = append(allMaps, currentMap)
-			currentMap = make([][]int, 0)
-			i += 2
-			continue
-		}
-
-		numsString := strings.Split(line, " ")
-		nums := make([]int, 0, 3)
-		for _, numString := range numsString {
-			num, _ := strconv.Atoi(numString)
-			nums = append(nums, num)
-		}
-		tmp := nums[0]
-		nums[0] = nums[1]
-		nums[1] = tmp
-		currentMap = append(currentMap, nums)
-		i++
-	}
-	allMaps = append(allMaps, currentMap)
-
-	for _, currentMap := range allMaps {
-		sort.SliceStable(currentMap, func(i, j int) bool {
-			return currentMap[i][0] < currentMap[j][0]
-		})
-	}
-
-	location := math.MaxInt
-	for i := 0; i < len(seeds); i += 2 {
-		seedStart := seeds[i]
-		seedRange := seeds[i+1]
-		for i := 0; i < seedRange; i++ {
-			val := seedStart + i
-			for _, currentMap := range allMaps {
-				index := findLastLessThanOrEqualTo(currentMap, 0, len(currentMap)-1, val)
-				if index != -1 {
-					entryMap := currentMap[index]
-					if val < entryMap[0]+entryMap[2] {
-						val = entryMap[1] + (val - entryMap[0])
-					}
-				}
-			}
-			if val < location {
-				location = val
+	// Find lowest location
+	lowestLocation := math.MaxInt
+	for _, s := range seeds {
+		for _, e := range mapping {
+			if s >= e.from.start && s <= e.from.end {
+				offset := e.to.start - e.from.start
+				location := s + offset
+				lowestLocation = min(location, lowestLocation)
 			}
 		}
 	}
 
-	return strconv.Itoa(location), nil
+	return strconv.Itoa(lowestLocation), nil
 }
 
 func findLastLessThanOrEqualTo(arr [][]int, l int, r int, n int) int {
@@ -160,4 +63,264 @@ func findLastLessThanOrEqualTo(arr [][]int, l int, r int, n int) int {
 	} else {
 		return findLastLessThanOrEqualTo(arr, l, m-1, n)
 	}
+}
+
+/*
+ * PART TWO
+ */
+
+func Part2(inputFileNumber int) (string, error) {
+	opts := utils.InputOptions{Day: 5, FileNumber: inputFileNumber}
+	lines, err := utils.ReadInput(opts)
+	if err != nil {
+		return "", err
+	}
+
+	// Get seeds
+	seedsString := strings.Split(lines[0], ": ")
+	seedsString = strings.Split(seedsString[1], " ")
+	seeds := make([]int, 0, len(seedsString))
+	for _, seedString := range seedsString {
+		seed, _ := strconv.Atoi(seedString)
+		seeds = append(seeds, seed)
+	}
+	seedRanges := []window{}
+	for i := 0; i < len(seeds); i += 2 {
+		seedRanges = append(seedRanges, window{seeds[i], seeds[i] + seeds[i+1] - 1})
+	}
+
+	// Build mapping from seed to location
+	mapping := buildMap(lines)
+
+	// Sort mapping based on 'to' ranges
+	sort.SliceStable(mapping, func(i, j int) bool {
+		return mapping[i].to.start < mapping[j].to.start
+	})
+
+	for _, e := range mapping {
+		lowestLocation := math.MaxInt
+		for _, s := range seedRanges {
+			// if there is an overlap
+			if s.start <= e.from.end && e.from.start <= s.end {
+				seed := max(s.start, e.from.start)
+				offset := e.to.start - e.from.start
+				location := seed + offset
+				lowestLocation = min(lowestLocation, location)
+			}
+		}
+		if lowestLocation != math.MaxInt {
+			return strconv.Itoa(lowestLocation), nil
+		}
+	}
+
+	// This behavior is NOT correct. It is possible that the answer
+	// has not been shifted at all. I just assumed that the answer
+	// has to be from a seed number whose location has been shifted,
+	// which was the case. If seed - location has not been shifted,
+	// then will have to search here instead of returning error.
+	return "", errors.New("Not able to find seed with overlap")
+}
+
+/*
+ * HELPER
+ */
+
+type window struct {
+	start int
+	end   int
+}
+
+type mapEntry struct {
+	from window
+	to   window
+}
+
+type listMap []mapEntry
+
+func buildMap(lines []string) listMap {
+	mapping := listMap{}
+	i := len(lines) - 1
+	for ; lines[i][0] >= '0' && lines[i][0] <= '9'; i-- {
+		entry := getMapEntryFromLine(lines[i])
+		mapping = append(mapping, entry)
+	}
+
+	// Sort mapping based on 'from' ranges
+	sort.SliceStable(mapping, func(i, j int) bool {
+		return mapping[i].from.start < mapping[j].from.start
+	})
+
+	// Start at the last entry of 'temperature-to-humidity map'
+	i -= 2
+
+	j := 0
+
+	currentMap := listMap{}
+	for i >= 2 {
+		line := lines[i]
+		if line[0] >= '0' && line[0] <= '9' {
+			// add entry to map
+			entry := getMapEntryFromLine(line)
+			currentMap = append(currentMap, entry)
+
+			// move up one line
+			i--
+			continue
+		}
+
+		// merge mappings
+		mapping = mergeMaps(currentMap, mapping)
+
+		// reset map for next iteration
+		currentMap = listMap{}
+		i -= 2
+		j++
+	}
+
+	return mapping
+}
+
+func getMapEntryFromLine(line string) mapEntry {
+	// Parse entry from string into list of numbers
+	unparsedNumbers := strings.Split(line, " ")
+	numbers := []int{}
+	for _, n := range unparsedNumbers {
+		num, _ := strconv.Atoi(n)
+		numbers = append(numbers, num)
+	}
+
+	// Populate mapping entry
+	fromEntry := window{numbers[1], numbers[1] + numbers[2] - 1}
+	toEntry := window{numbers[0], numbers[0] + numbers[2] - 1}
+	return mapEntry{fromEntry, toEntry}
+}
+
+func mergeMaps(fromMap, toMap listMap) listMap {
+	// Sort fromMap based on 'to' ranges
+	sort.SliceStable(fromMap, func(i, j int) bool {
+		return fromMap[i].to.start < fromMap[j].to.start
+	})
+
+	// Sort toMap based on 'from' ranges
+	sort.SliceStable(toMap, func(i, j int) bool {
+		return toMap[i].from.start < toMap[j].from.start
+	})
+
+	// Combine intervals
+	combinedWindows := []window{}
+	for i := 0; i < len(fromMap); i++ {
+		fromWindow := fromMap[i].to
+		combinedWindows = append(combinedWindows, fromWindow)
+	}
+	for i := 0; i < len(toMap); i++ {
+		toWindow := toMap[i].from
+		combinedWindows = append(combinedWindows, toWindow)
+	}
+	slices.SortFunc(combinedWindows, func(i, j window) int {
+		return i.start - j.start
+	})
+
+	// Initialize checkpoints and intervals
+	a, b := combinedWindows[0].start, combinedWindows[0].end
+	checkpointSet := map[int]bool{
+		a:     true,
+		b + 1: true,
+	}
+	intervals := []window{{a, b + 1}}
+	checkpoints := []int{}
+
+	// Populate intervals
+	for _, w := range combinedWindows[1:] {
+		a, b := w.start, w.end
+		checkpointSet[a] = true
+		checkpointSet[b+1] = true
+		if a <= intervals[len(intervals)-1].end {
+			last := len(intervals) - 1
+			intervals[last].end = max(intervals[last].end, b+1)
+		} else {
+			intervals = append(intervals, window{a, b + 1})
+		}
+	}
+
+	// Populate checkpoints
+	for k := range checkpointSet {
+		checkpoints = append(checkpoints, k)
+	}
+	slices.Sort(checkpoints)
+
+	// Build non-overlapping intervals
+	intermediateWindows := []window{}
+	intervalIndex := 0
+	lastCheckpoint := checkpoints[0]
+	for _, checkpoint := range checkpoints[1:] {
+		end := checkpoint - 1
+		for end >= intervals[intervalIndex].end {
+			intervalIndex++
+		}
+		if end >= intervals[intervalIndex].start {
+			w := window{lastCheckpoint, end}
+			intermediateWindows = append(intermediateWindows, w)
+		}
+		lastCheckpoint = checkpoint
+	}
+
+	mergedMap := listMap{}
+	fromIndex, toIndex := 0, 0
+	for _, w := range intermediateWindows {
+		for fromIndex < len(fromMap) && w.end > fromMap[fromIndex].to.end {
+			fromIndex++
+		}
+		for toIndex < len(toMap) && w.end > toMap[toIndex].from.end {
+			toIndex++
+		}
+
+		var fromWindow window
+		if fromIndex >= len(fromMap) || w.start < fromMap[fromIndex].to.start {
+			// default 'from' mapping
+			fromWindow = window{w.start, w.end}
+		} else {
+			offset := fromMap[fromIndex].to.start - fromMap[fromIndex].from.start
+			fromWindow = window{w.start - offset, w.end - offset}
+		}
+
+		var toWindow window
+		if toIndex >= len(toMap) || w.start < toMap[toIndex].from.start {
+			// default 'to' mapping
+			toWindow = window{w.start, w.end}
+		} else {
+			offset := toMap[toIndex].to.start - toMap[toIndex].from.start
+			toWindow = window{w.start + offset, w.end + offset}
+		}
+
+		mergedMap = append(mergedMap, mapEntry{
+			fromWindow,
+			toWindow,
+		})
+	}
+
+	finalMap := listMap{}
+	for _, e := range mergedMap {
+		from := e.from
+		to := e.to
+		if from.start == to.start && from.end == to.end {
+			continue
+		}
+		finalMap = append(finalMap, e)
+	}
+
+	return finalMap
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
 }
