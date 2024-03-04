06966c86e0ae7caec73b53c09a9317a79caa083e
Tee Amornkasemwong <tamornkasemwong@netflix.com>
2024-03-03 20:40:13 -0800

    added day 7 template

---
+++ day7/day07.go
@@ -0,0 +1,21 @@
+package day7
+
+import (
+	"fmt"
+
+	"github.com/TeeAmorn/aoc-go-2023/utils"
+)
+
+func Part1(inputFileNumber int) (string, error) {
+	opts := utils.InputOptions{Day: 7, FileNumber: inputFileNumber}
+	lines, _ := utils.ReadInput(opts)
+	fmt.Println(lines)
+	return "", nil
+}
+
+func Part2(inputFileNumber int) (string, error) {
+	opts := utils.InputOptions{Day: 7, FileNumber: inputFileNumber}
+	lines, _ := utils.ReadInput(opts)
+	fmt.Println(lines)
+	return "", nil
+}
