dc0875fc8c2956186583d7b06c4b5b3c0cd9e545
Tee Amornkasemwong <tamornkasemwong@netflix.com>
2024-03-09 15:09:03 -0800

    completed day 9

--- day09/day09.go
+++ day09/day09.go
@@ -1,7 +1,8 @@
 package day09
 
 import (
-	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/TeeAmorn/aoc-go-2023/utils"
 )
@@ -14,8 +15,18 @@ func Part1(inputFileNumber int) (string, error) {
 	opts := utils.InputOptions{Day: 9, FileNumber: inputFileNumber}
 	lines, _ := utils.ReadInput(opts)
 
-	fmt.Println(lines)
-	return "", nil
+	answer := 0
+	for _, l := range lines {
+		numbers := strings.Split(l, " ")
+		s := []int{}
+		for _, n := range numbers {
+			number, _ := strconv.Atoi(n)
+			s = append(s, number)
+		}
+		answer += extrapolateLast(s)
+	}
+
+	return strconv.Itoa(answer), nil
 }
 
 /*
@@ -26,6 +37,58 @@ func Part2(inputFileNumber int) (string, error) {
 	opts := utils.InputOptions{Day: 9, FileNumber: inputFileNumber}
 	lines, _ := utils.ReadInput(opts)
 
-	fmt.Println(lines)
-	return "", nil
+	answer := 0
+	for _, l := range lines {
+		numbers := strings.Split(l, " ")
+		s := []int{}
+		for _, n := range numbers {
+			number, _ := strconv.Atoi(n)
+			s = append(s, number)
+		}
+		answer += extrapolateFirst(s)
+	}
+
+	return strconv.Itoa(answer), nil
+}
+
+/*
+ * HELPER
+ */
+
+func extrapolateLast(sequence []int) int {
+	// Terminal case: If sequence is an array of 0, return 0
+	found := false
+	for i := 0; i < len(sequence); i++ {
+		if sequence[i] != 0 {
+			found = true
+		}
+	}
+	if !found {
+		return 0
+	}
+
+	s := []int{}
+	for i := 1; i < len(sequence); i++ {
+		s = append(s, sequence[i]-sequence[i-1])
+	}
+	return extrapolateLast(s) + sequence[len(sequence)-1]
+}
+
+func extrapolateFirst(sequence []int) int {
+	// Terminal case: If sequence is an array of 0, return 0
+	found := false
+	for i := 0; i < len(sequence); i++ {
+		if sequence[i] != 0 {
+			found = true
+		}
+	}
+	if !found {
+		return 0
+	}
+
+	s := []int{}
+	for i := 1; i < len(sequence); i++ {
+		s = append(s, sequence[i]-sequence[i-1])
+	}
+	return sequence[0] - extrapolateFirst(s)
 }
